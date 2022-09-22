db77926e7d8e0bddf7c70b89bc87b076d8f0b8b2
groot <diopm.contact@gmail.com>
2022-09-23 01:22:33 +0200

    semi-working chat

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Client {
-}
+import java.net.* ;
+import java.io.* ;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String [] args ) {
+        String address = "127.0.0.1";
+        String nameDefault = "GigaChad";
+        int idClient;
+        try {// Open sockets and streams
+            Socket sock = new Socket(address,10000);
+            DataOutputStream out =new DataOutputStream(sock.getOutputStream());
+            DataInputStream in = new DataInputStream(sock.getInputStream());
+            if (args.length > 0) // Send name
+                out.writeUTF(args[0]);
+            else
+                out.writeUTF(nameDefault);
+            idClient= in.readInt(); // number reception
+            System.out.println("Player n°: "+idClient);
+
+            String message="";
+            Scanner scanner = new Scanner(System.in);
+            while(!message.equals("end")){
+                message = scanner.nextLine();
+                out.writeUTF(message);
+            }
+            System.out.println("Chat closed.");
+            in.close(); // Close stream
+            out.close();
+            sock.close() ; // Close socket
+        } catch (UnknownHostException e) {System.out.println(address + " unreachable.");}
+        catch (IOException e) {e.printStackTrace();}
+    }
+} 
--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,61 @@
-package PACKAGE_NAME;public class Server {
+import java.net.* ; // Sockets
+import java.io.* ; // Streams
+public class Server implements Runnable{
+
+    private ServerSocket gestSock;
+    private static final int nThread = 5;
+    private static Thread[] clientThread = new Thread[nThread];
+
+    public static void main(String [] args) {
+        new Server();
+
+    }
+    Server(){
+        System.out.println("Server starting") ;
+        try {// Socket manager : port 10000
+            gestSock = new ServerSocket(10000);
+            for (int i = 0; i < nThread; i++) {
+                clientThread[i] = new Thread(this);
+                clientThread[i].start();
+            }
+        } catch (IOException e) {e.printStackTrace( );}
+        //gestSock.close();
+    }
+    @Override
+    public void run() {
+        System.out.println("oui");
+        Socket socket = null; // Wait
+        DataInputStream entree;
+        DataOutputStream sortie;
+        try {
+            socket = gestSock.accept();
+            entree = new DataInputStream(socket.getInputStream());
+            sortie = new DataOutputStream(socket.getOutputStream());
+// Data reading
+            String namePlayer = entree.readUTF();
+            System.out.println(namePlayer + " is connected");
+// Send data : 0 for instance
+            sortie.writeInt(0);
+
+            String message = "";
+            while (!message.equals("end")) {
+                try {
+                    message = entree.readUTF();
+                    System.out.println("[" + namePlayer + "]: " + message);
+                } catch (EOFException e) {
+                    message = "end";
+                }
+                catch (SocketException s){
+                    message = "end";
+                }
+            }
+            System.out.println(namePlayer + " has disconnected");
+            // Quick cleaning
+            sortie.close();
+            entree.close();
+            socket.close();
+        } catch (IOException e) {// Quick cleaning
+            throw new RuntimeException();
+        }
+    }
 }
